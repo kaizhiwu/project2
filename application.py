1e404a7c13a1b1e4d56371cf59ce9b7cb77242eb
Kaizhi Wu <kwu4@uvm.edu>
2018-07-24 20:26:26 +0000

    final version

--- application.py
+++ application.py
@@ -8,13 +8,33 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # list of all channels
-channel_list = ['general']
+channel_message = {}
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",channel_list = channel_message.keys())
+
+@app.route("/<string:channel2display>", methods=["POST"])
+def display(channel2display):
+    if channel2display in channel_message:
+        return jsonify({"success": True, "message":channel_message[channel2display]})
+    else:
+        channel_message[channel2display] = ['second default message','first']
+        return jsonify({"success": True, "message":channel_message[channel2display]})
 
 @socketio.on("create channel")
-def channel(new_channel):
-    channel_list.append(new_channel)
-    emit("display channels", channel_list, broadcast=True)
+def channel(data):
+    new_channel = data["new_channel"]
+    message = 'first message'
+    channel_message[new_channel] = [message]
+    emit("display channels", channel_message.keys(), broadcast=True)
+
+@socketio.on("submit message")
+def append_message(data):
+    channel = data["channel"]
+    text = data["message"]
+    time = data["time"]
+    user = data["user"]
+    message = text
+    channel_message[channel].append(message)
+    emit("new_message_added", channel_message[channel], broadcast=True)
--- static/index.js
+++ static/index.js
@@ -21,10 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
     };
 
+
     //create a new chatting channel.
     document.querySelector('#create_channel').onsubmit = () => {
         const new_channel = document.querySelector('#new_channel').value;
-        socket.emit("create channel", new_channel);
+        //communicate with the server.
+        socket.emit("create channel", {'new_channel': new_channel});
         //clear the input
         document.querySelector('#new_channel').value = '';
         // Stop form from submitting
@@ -32,12 +34,71 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     //create and display all the chatting list.
-    socket.on("display channels", data => {
-        for (var channel in data) {
+    socket.on("display channels", channelList => {
+        //empty the selecting box.
+        document.getElementById("choose_channels").innerHTML = "";
+        //append all the options.
+        for (var channel of channellist) {
             const channel_option = document.createElement('option');
-            channel_option.innerHTMl = channel;
-            channel_option.dataset.channel = channel;
+            channel_option.innerHTML = channel;
+            channel_option.value = channel;
             document.querySelector('#choose_channels').append(channel_option);
+        }
+    });
+
+    //select the channel and fetch the messsage stored in that channel.
+    document.querySelector('#choose_channels').onchange = function() {
+        // Initialize new request
+        const request = new XMLHttpRequest();
+        const channel2display = this.value;
+        request.open('POST', `/${channel2display}`);
+
+        //callback function when the request completes.
+        request.onload = () => {
+            //extracts JSON data from request
+            const data = JSON.parse(request.responseText);
+            // Update the result div
+            if (data.success) {
+                const contents = data.message;
+                document.querySelector('#channel_message').innerHTML = '';
+                for (var message of contents) {
+                    const li = document.createElement('li');
+                    li.innerHTML = message;
+                    document.querySelector('#channel_message').append(li);
+                }}
+            else {
+                document.querySelector('#channel_message').innerHTML = 'There was an error.';
+                }
+            };
+            // Add data to send with request
+            const data = new FormData();
+            data.append('channel2display', channel2display);
+
+            // Send request
+            request.send(data);
+            return false;
         };
+
+    //create a new message.
+    document.querySelector('#submit_message').onsubmit = () => {
+        const message = document.querySelector('#new_message').value;
+        const channel = document.querySelector('#choose_channels').value;
+        var d = new Date(); // get the system date.
+        var user = localStorage.getItem('username');
+        socket.emit("submit message", {'channel': channel, 'message':message,'time':d,'user':user});
+        //clear the input.
+        document.querySelector('#new_message').value = '';
+        // Stop form from submitting.
+        return false;
+    };
+
+    //display the new message.
+    socket.on("new_message_added",data =>{
+        document.querySelector('#channel_message').innerHTML = '';
+        for (var message of data) {
+            const li = document.createElement('li');
+            li.innerHTML = message;
+            document.querySelector('#channel_message').append(li);
+        }
     });
-    });
+});
